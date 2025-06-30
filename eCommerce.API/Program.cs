caaf4957a36af37a768fe0ec3e004e2f044b2942
yadav <yadav@CA>
2025-06-30 22:53:23 +0530

    12

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -1,6 +1,26 @@
+using eCommerce.Core;
+using eCommerce.Infrastructure;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add Infrastructure Services
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+// Add Controllers to the service collections
+builder.Services.AddControllers();
+
+// Build the web application
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Routing
+app.UseRouting();
 
+//Auth
+app.UseAuthentication();
+app.UseAuthorization();
+
+//Controller Routes
+app.MapControllers();
 app.Run();
---
+++ eCommerce.Core/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace eCommerce.Core
+{
+    public static class DependencyInjection
+    {
+        /// <summary>
+        ///  Extension Method to add core services to the dependency injection container.      
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddCore(this IServiceCollection services)
+        {
+
+            //TO DO: Add Services to the IOC Container
+            //Core services often include data access, caching and other low-level compnents.
+            return services;
+        }
+    }
+}
