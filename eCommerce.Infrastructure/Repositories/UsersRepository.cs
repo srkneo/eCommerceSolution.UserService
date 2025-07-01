aa05de429d67df655fe566543cdaccc9fcfbe4ad
yadav <yadav@CA>
2025-07-01 22:48:34 +0530

    23

--- eCommerce.Infrastructure/Repositories/UsersRepository.cs
+++ eCommerce.Infrastructure/Repositories/UsersRepository.cs
@@ -1,7 +1,9 @@
-﻿using eCommerce.Core.DTO;
+﻿using Dapper;
+using eCommerce.Core.DTO;
 using eCommerce.Core.Entities;
 using eCommerce.Core.RepositoryContracts;
 using eCommerce.Infrastructure.DbContext;
+using Npgsql;
 
 namespace eCommerce.Infrastructure.Repositories;
 
@@ -17,10 +19,21 @@ internal class UsersRepository : IUsersRepository
 
     public async Task<ApplicationUser?> AddUser(ApplicationUser user)
     {
+
         //Generate a new unique user ID for the user
         user.UserID = Guid.NewGuid();
 
-        return user;
+        // SQL Query to insert user data into the "Users" table.
+        // Correct SQL Query to insert user data into the "Users" table.
+        string query = "INSERT INTO public.\"Users\" (\"UserID\", \"Email\", \"PersonName\", \"Gender\", \"Password\") " +
+                       "VALUES (@UserID, @Email, @PersonName, @Gender, @Password)";
+        int rowCountAffected = await _dbContext.dbConnection.ExecuteAsync(query, user);
+        if (rowCountAffected > 0)
+        {
+            return user;
+        }
+        
+        return null;
     }
 
     public async Task<ApplicationUser?> GetUserByEmailAndPassword(string? email, string? password)
