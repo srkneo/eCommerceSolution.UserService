38da2c1ec1aa10b05b7fbeffcd4cf5ac7783287c
max yad <yadavmaheshing@gmail.com>
2025-07-20 13:54:23 +0530

    dapper changes

--- eCommerce.Infrastructure/DbContext/DapperDbContext.cs
+++ eCommerce.Infrastructure/DbContext/DapperDbContext.cs
@@ -7,25 +7,21 @@ namespace eCommerce.Infrastructure.DbContext;
 public class DapperDbContext
 {
     private readonly IConfiguration _configuration;
-    private readonly IDbConnection _connection;
+    private readonly string _connectionString;
+
     public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
 
         string connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection")!;
 
-        string connectionString = connectionStringTemplate
+        _connectionString = connectionStringTemplate
             .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
             .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"))
             .Replace("$POSTGRES_DATABASE", Environment.GetEnvironmentVariable("POSTGRES_DATABASE"))
             .Replace("$POSTGRES_PORT", Environment.GetEnvironmentVariable("POSTGRES_PORT"))
             .Replace("$POSTGRES_USER", Environment.GetEnvironmentVariable("POSTGRES_USER"));
-
-        //Create a new NpgsqlConnection with the retrived connection string
-
-        _connection = new NpgsqlConnection(connectionString);
-        
     }
 
-    public IDbConnection dbConnection => _connection;
+    public IDbConnection dbConnection => new NpgsqlConnection(_connectionString);
 }
